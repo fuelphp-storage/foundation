4de25509468c2feec437399ca719ea01479f2d98
WanWizard <wanwizard@wanwizard.eu>
2013-07-30 15:32:24 +0200

    added the Format facade

---
+++ src/Fuel/Foundation/Facades/Format.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package    Fuel\Foundation
+ * @version    2.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Fuel\Foundation\Facades;
+
+/**
+ * Format Facade class
+ *
+ * @package  Fuel\Foundation
+ *
+ * @since  1.0.0
+ */
+class Format extends Base
+{
+	/**
+	 * Returns an instance of the Format object.
+	 *
+	 *     echo Format::forge(array('foo' => 'bar'))->toXml();
+	 *
+	 * @param   mixed  general date to be converted
+	 * @param   string  data format the file was provided in
+	 * @return  Format
+	 */
+	public static function forge($data = null, $from_type = null)
+	{
+		return \Dependency::resolve('format', array($data, $from_type, \Config::load('format', true) ?: array(), \Input::getInstance()));
+	}
+}
