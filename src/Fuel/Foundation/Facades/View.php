5c7c7ca2961a7b29aed53cc806ca9bde4f935687
WanWizard <wanwizard@wanwizard.eu>
2013-07-30 15:47:15 +0200

    implemented View::setGlobal/bindGlobal

--- src/Fuel/Foundation/Facades/View.php
+++ src/Fuel/Foundation/Facades/View.php
@@ -19,6 +19,37 @@ namespace Fuel\Foundation\Facades;
  */
 class View extends Base
 {
+	/**
+	 * Sets a global variable, except that the variable will be
+	 * accessible to all views and presenters.
+	 *
+	 *     View::setGlobal($name, $value);
+	 *
+	 * @param   string  variable name or an array of variables
+	 * @param   mixed   value
+	 * @param   bool    whether to filter the data or not
+	 * @return  void
+	 */
+	public static function setGlobal($key, $value = null, $filter = null)
+	{
+		return static::getInstance()->set($key, $value, $filter);
+	}
+
+	/**
+	 * Assigns a global variable by reference, except that the variable
+	 * will be accessible to all views and presenters
+	 *
+	 *     View::bindGlobal($key, $value);
+	 *
+	 * @param   string  variable name
+	 * @param   mixed   referenced variable
+	 * @param   bool    whether to filter the data or not
+	 * @return  void
+	 */
+	public static function bindGlobal($key, &$value, $filter = null)
+	{
+		return static::getInstance()->bind($key, $value, $filter);
+	}
 
 	/**
 	 * Get the active applications View Manager
