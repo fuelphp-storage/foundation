5f5cf4a2e8d1d91ab60c2502feec0a7b83be65d1
WanWizard <wanwizard@wanwizard.eu>
2013-08-13 15:40:28 +0200

    detect requests to an already loaded presenter

--- src/Fuel/Foundation/Facades/Presenter.php
+++ src/Fuel/Foundation/Facades/Presenter.php
@@ -46,18 +46,23 @@ class Presenter extends Base
 		$segments = explode('/', $presenter);
 		while(count($segments))
 		{
-			$file = $path.'classes'.DS.'Presenter'.DS.implode('/', array_map('ucfirst', $segments)).'.php';
-			if (file_exists($file))
-			{
-				include $file;
+			$class = $namespace.'Presenter\\'.implode('\\', array_map('ucfirst', $segments));
 
-				$class = $namespace.'Presenter\\'.implode('\\', array_map('ucfirst', $segments));
-				if (class_exists($class))
+			if ( ! class_exists($class, false))
+			{
+				$file = $path.'classes'.DS.'Presenter'.DS.implode('/', array_map('ucfirst', $segments)).'.php';
+				if (file_exists($file))
 				{
-					$presenter = new $class(\View::getInstance(), $method, $autoFilter, $view);
-					break;
+					include $file;
 				}
 			}
+
+			if (class_exists($class))
+			{
+				$presenter = new $class(\View::getInstance(), $method, $autoFilter, $view);
+				break;
+			}
+
 			array_pop($segments);
 		}
 
