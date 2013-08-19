10cea620bd46c4b7a39238eb547f27cc0f230f26
WanWizard <wanwizard@wanwizard.eu>
2013-08-19 14:41:09 +0200

    added default lang files

---
+++ defaults/lang/en/byteunits.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package    demo-application
+ * @version    2.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * Valid byte units => power of 2 that defines the unit's size
+ *
+ * @var		array
+ */
+return array(
+	'B'   => 0,
+	'K'   => 10,
+	'Ki'  => 10,
+	'KB'  => 10,
+	'KiB' => 10,
+	'M'   => 20,
+	'Mi'  => 20,
+	'MB'  => 20,
+	'MiB' => 20,
+	'G'   => 30,
+	'Gi'  => 30,
+	'GB'  => 30,
+	'GiB' => 30,
+	'T'   => 40,
+	'Ti'  => 40,
+	'TB'  => 40,
+	'TiB' => 40,
+	'P'   => 50,
+	'Pi'  => 50,
+	'PB'  => 50,
+	'PiB' => 50,
+	'E'   => 60,
+	'Ei'  => 60,
+	'EB'  => 60,
+	'EiB' => 60,
+	'Z'   => 70,
+	'Zi'  => 70,
+	'ZB'  => 70,
+	'ZiB' => 70,
+	'Y'   => 80,
+	'Yi'  => 80,
+	'YB'  => 80,
+	'YiB' => 80,
+);
---
+++ defaults/lang/en/date.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package    demo-application
+ * @version    2.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+return array(
+	'text' => ':time ago',
+
+	'second' => ':t second',
+	'seconds' => ':t seconds',
+
+	'minute' => ':t minute',
+	'minutes' => ':t minutes',
+
+	'hour' => ':t hour',
+	'hours' => ':t hours',
+
+	'day' => ':t day',
+	'days' => ':t days',
+
+	'week' => ':t week',
+	'weeks' => ':t weeks',
+
+	'month' => ':t month',
+	'months' => ':t months',
+
+	'year' => ':t year',
+	'years' => ':t years',
+
+	'decade' => ':t decade',
+	'decades' => ':t decades',
+);
