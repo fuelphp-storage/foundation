9fcf7a60b0e91788b21fdb3268d47f0a2b23a0e1
WanWizard <wanwizard@wanwizard.eu>
2013-11-12 22:58:50 +0100

    added missing return value to create

--- src/Fuel/Foundation/Session/Memcached.php
+++ src/Fuel/Foundation/Session/Memcached.php
@@ -82,8 +82,10 @@ class Memcached extends Driver
 			$this->regenerate();
 
 			// and start the session
-			$this->start();
+			return $this->start();
 		}
+
+		return false;
 	}
 
     /**
@@ -192,6 +194,7 @@ class Memcached extends Driver
      * Destroy the session
      *
      * @return bool  result of the write operation
+     *
 	 * @since  2.0.0
      */
     public function destroy()
