3c45702436e4e0431e3211a7f0b01f1afff3d1ef
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-02-26 20:28:25 +0100

    Moves proxies to legacy package, closes #18

--- src/Providers/FuelLibraryProvider.php
+++ src/Providers/FuelLibraryProvider.php
@@ -4,7 +4,7 @@
  * @version    2.0
  * @author     Fuel Development Team
  * @license    MIT License
- * @copyright  2010 - 2014 Fuel Development Team
+ * @copyright  2010 - 2015 Fuel Development Team
  * @link       http://fuelphp.com
  */
 
@@ -13,66 +13,22 @@ namespace Fuel\Foundation\Providers;
 use Fuel\Foundation\LibraryProvider;
 
 /**
- * FuelPHP LibraryProvider class for this composer library
- *
- * @package  Fuel\Foundation
- *
- * @since  2.0.0
+ * Fuel LibraryProvider class for Foundation
  */
 class FuelLibraryProvider extends LibraryProvider
 {
 	/**
-	 * Library initialization method. This method is called as soon as the library
-	 * is initially loaded, either by the framework bootstrap, or when you manually
-	 * load a new library into the autoloader using the applications getLibrary() method.
-	 *
-	 * @since 2.0.0
+	 * {@inheritdoc}
 	 */
 	public function initialize()
 	{
 		// fetch the alias instance
-		$alias = $this->dic->get('alias');
+		$alias = $this->container->get('alias');
 
-		/**
-		 * Alias the Fuel class to global
-		 */
+		// alias the Fuel class to global
 		$alias->alias('Fuel', 'Fuel\Foundation\Fuel');
 
-		/**
-		 * Alias all Foundation proxy classes to global
-		 */
-		$alias->aliasNamespace('Fuel\Foundation\Proxy', '');
-
-		/**
-		 * Alias the base controllers to the Fuel\Controller namespace
-		 */
+		// alias the base controllers to the Fuel\Controller namespace
 		$alias->aliasNamespace('Fuel\Foundation\Controller', 'Fuel\Controller');
-	}
-
-	/**
-	 * Library enabler method. By default, a loaded library is disabled.
-	 * When you instruct your application to use the library, this enabler gets
-	 * called. You can use it to prep the application for use of the library.
-	 *
-	 * @param  Application  $app  The application instance that wants to enable this library
-	 *
-	 * @since 2.0.0
-	 */
-	public function enable($app)
-	{
-	}
-
-	/**
-	 * Library disabler method. By default, a loaded library is disabled.
-	 * When you instruct your application to unload a library, this disabler gets
-	 * called. You can use it to cleanup any setup the library has made in the
-	 * application that was using it.
-	 *
-	 * @param  Application  $app  The application instance that had enabled this library
-	 *
-	 * @since 2.0.0
-	 */
-	public function disable($app)
-	{
 	}
 }
