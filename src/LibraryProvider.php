e8b95b46f9c3d8fa005a06afbd0694be4e2151ad
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-02-26 15:56:36 +0100

    Refactors the library provider

--- src/LibraryProvider.php
+++ src/LibraryProvider.php
@@ -4,48 +4,46 @@
  * @version    2.0
  * @author     Fuel Development Team
  * @license    MIT License
- * @copyright  2010 - 2014 Fuel Development Team
+ * @copyright  2010 - 2015 Fuel Development Team
  * @link       http://fuelphp.com
  */
 
 namespace Fuel\Foundation;
 
+use Fuel\Dependency\Container;
+
 /**
  * LibraryProvider base class
- *
- * @package  Fuel\Foundation
- *
- * @since  2.0.0
  */
-class LibraryProvider
+abstract class LibraryProvider
 {
 	/**
-	 * @var  Fuel\Dependency\Container  Fuel's DiC
+	 * @var Container
 	 */
-	protected $dic;
+	protected $container;
 
 	/**
-	 * @var  string  base namespace of this package
+	 * @var string
 	 */
 	protected $namespace;
 
 	/**
-	 * @var  array  array of paths defined for this namespace
+	 * @var array
 	 */
-	protected $paths = array();
+	protected $paths = [];
 
 	/**
-	 * Class constructor, initialize the package, check for existence of a
+	 * Initialize the package, check for existence of a
 	 * bootstrap file, and if found, process it
 	 *
-	 * @param
-	 *
-	 * @since 2.0.0
+	 * @param Container $container
+	 * @param string    $namespace
+	 * @param array     $paths
 	 */
-	public function __construct($dic, $namespace, Array $paths = array())
+	public function __construct(Container $container, $namespace, array $paths = [])
 	{
 		// store the DiC
-		$this->dic = $dic;
+		$this->container = $container;
 
 		// normalize the namespace and store it
 		$this->namespace = trim($namespace, '\\').'\\';
@@ -64,40 +62,34 @@ class LibraryProvider
 	 * Library's initialization method. This method is called as soon as the library
 	 * is initially loaded, either by the framework bootstrap, or when you manually
 	 * load a new library into the autoloader using the Application's getLibrary() method.
-	 *
-	 * @since 2.0.0
 	 */
 	public function initialize()
 	{
 	}
 
 	/**
-	 * Library enabler method.
+	 * Library enabler method
 	 *
 	 * When you instruct your application to use the library, this enabler gets
 	 * called. You can use it to prep the application for use of the library.
 	 * By default, a loaded library is disabled.
 	 *
-	 * @param  Application  $app  The application instance that wants to enable this library
-	 *
-	 * @since 2.0.0
+	 * @param Application $app
 	 */
-	public function enable($app)
+	public function enable(Application $app)
 	{
 	}
 
 	/**
-	 * Library disabler method.
+	 * Library disabler method
 	 *
 	 * When you instruct your application to unload a library, this disabler gets
 	 * called. You can use it to cleanup any setup the library has made in the
 	 * application that was using it.
 	 *
-	 * @param  Application  $app  The application instance that had enabled this library
-	 *
-	 * @since 2.0.0
+	 * @param Application $app
 	 */
-	public function disable($app)
+	public function disable(Application $app)
 	{
 	}
 }
