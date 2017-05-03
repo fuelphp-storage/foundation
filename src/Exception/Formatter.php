c5a83b1c8a853eb00d66a288e327a3a01e5ad535
stevewest <steven.david.west@gmail.com>
2017-05-03 12:48:02 +0100

    Creates initial formatter class for #31

---
+++ src/Exception/Formatter.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package    Fuel\Foundation
+ * @version    2.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2017 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Fuel\Foundation\Exception;
+
+use RuntimeException;
+
+class Formatter extends RuntimeException
+{
+
+}
